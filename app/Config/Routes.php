ff0053731ff15bf49b1d8ddc4767753968659923
augustofanck <augustofanck14@gmail.com>
2025-09-03 20:57:15 -0300

    feat: initial commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,10 +5,25 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
+// Dashboard
+
 $routes->get('/', 'Dashboard::index');
+
+// Ordens
+
 $routes->get('ordens', 'Ordem::index');
 $routes->get('ordens/create', 'Ordem::create');
 $routes->post('ordens', 'Ordem::store');
 $routes->get('ordens/(:num)/edit', 'Ordem::edit/$1');
 $routes->post('ordens/(:num)/update', 'Ordem::update/$1');
 $routes->get('ordens/(:num)/delete', 'Ordem::delete/$1');
+
+// Clientes
+
+$routes->get('clientes', 'Clientes::index');
+$routes->get('clientes/create', 'Clientes::create');
+$routes->post('clientes/store', 'Clientes::store');
+$routes->get('clientes/(:num)/edit', 'Clientes::edit/$1');
+$routes->post('clientes/(:num)/update', 'Clientes::update/$1');
+$routes->get('clientes/(:num)/delete', 'Clientes::delete/$1');
--- app/Models/ClienteModel.php
+++ app/Models/ClienteModel.php
@@ -6,14 +6,26 @@ use CodeIgniter\Model;
 
 class ClienteModel extends Model
 {
-    protected $table      = 'clientes';    // nome da tabela
-    protected $primaryKey = 'id';
-
-    protected $allowedFields = [
+    protected $table            = 'clientes';
+    protected $primaryKey       = 'id';
+    protected $useTimestamps    = true;
+    protected $useSoftDeletes   = true;
+    protected $allowedFields    = [
         'nome',
-        'cnpj_cpf',
-        'termino_contrato',
-        'created_at',
-        'updated_at'
+        'documento',
+        'email',
+        'telefone',
+        'endereco',
+        'cidade',
+        'estado',
+        'cep',
+        'termino_contrato'
+    ];
+
+    protected $validationRules = [
+        'nome'  => 'required|min_length[2]|max_length[150]',
+        'email' => 'permit_empty|valid_email|max_length[150]',
+        'estado' => 'permit_empty|exact_length[2]',
+        'cep'   => 'permit_empty|max_length[9]',
     ];
 }
